a4c941ee959a59f6042839a1d4fc03603a490d01
elenazaharova <elena.zakharova@simbirsoft.com>
2024-10-12 21:18:57 +0300

    Initial commit

---
+++ 14.1.py
@@ -0,0 +1,34 @@
+#создание_класса
+class Product:
+    pass
+
+    name = str
+    description = str
+    price = float
+    quantity = int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+
+#name = Product()  #
+        #description = Product()  #
+        #price = Product()  #
+        #quantity = Product()  #
+
+
+#создание_класса
+class Category:
+    name = str
+    description = str
+    products = str
+
+name = Category()
+description = Category()
+products = Category()
+
+
+
