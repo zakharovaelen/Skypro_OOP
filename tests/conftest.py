ba8697d22c3417596793de8365ad37a564d7f67f
elenazaharova <elena.zakharova@simbirsoft.com>
2024-10-13 17:04:53 +0300

    j

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,35 @@
 import pytest
 
-from Product import Product
-from 14.1_main import Category
+from src.product import Product
+from src.category import Category
+
 
 @pytest.fixture
-
 def first_product():
     return Product(
-        name="name",
-        description="description",
-        price="price",
-        quantity="quantity",
+        name="Samsung Galaxy S23 Ultra",
+        description="256GB, Серый цвет, 200MP камера",
+        price=180000.0,
+        quantity=5
+    )
+@pytest.fixture
+def product():
+    return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+
+@pytest.fixture
+def first_category():
+    return Category(
+        name="Смартфоны",
+        description="Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        products= [
+            Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
+            Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
+            Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)]
+
     )
 
-
+'''@pytest.fixture
+def category():
+    return Category("Смартфоны",
+                    "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+                    [products])'''
---
+++ tests/test_product.py
@@ -0,0 +1,5 @@
+def test_product_init(product):
+    assert product.name == "Samsung Galaxy S23 Ultra"
+    assert product.description == "256GB, Серый цвет, 200MP камера"
+    assert product.price == 180000.0
+    assert product.quantity == 5
